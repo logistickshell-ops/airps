f0453cc565b20886b8f6d7dd3814b1afc7a0b46b
Disa_ad <logistickshell@gmail.com>
2026-04-20 20:47:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import { viteSingleFile } from "vite-plugin-singlefile"; // <--- ЗАКОММЕНТИРОВАЛИ
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: './', // <--- ОБЯЗАТЕЛЬНО ДОБАВЬ ЭТО
-  plugins: [react(), tailwindcss()], // <--- УБРАЛИ singlefile
+  base: './', // Критически важно для относительных путей
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
